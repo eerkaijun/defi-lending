ad0462dc19f9fe778726af8ebea3b956a01942b6
eerkaijun <kaijuneer@gmail.com>
2022-10-20 16:44:45 +0100

    escrow contract

---
+++ test/test-escrow.js
@@ -0,0 +1,17 @@
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
+
+describe("Escrow", () => {
+
+    it("Should be able to deposit", async () => {
+        let user;
+        [user] = await ethers.getSigners();
+        const Escrow = await ethers.getContractFactory("Escrow");
+        const escrow = await Escrow.deploy();
+
+        const depositAmount = ethers.utils.parseEther("0.5");
+        escrow.connect(user).lend({value: depositAmount});
+
+        expect(await escrow.deposits(user.address)).equal(depositAmount);
+    });
+});
