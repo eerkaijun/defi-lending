b6f11fd49b3cef9a0b194b0fec981e4c8e28a5c9
eerkaijun <kaijuneer@gmail.com>
2022-10-25 11:19:14 +0100

    allow withdrawal in escrow

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,13 +1,35 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import { NotAdmin } from "./interfaces/Errors.sol";
 
 contract Escrow {
+
+    address admin;
+    bool allowWithdrawal = false;
     mapping(address => uint256) public deposits;
+
+    modifier onlyAdmin() {
+        if (msg.sender != admin) {
+            revert NotAdmin(msg.sender, admin);
+        }
+        _;
+    }
+
+    constructor() {
+        admin = msg.sender;
+    }
 
     function lend() external payable {
         deposits[msg.sender] += msg.value;
     }
+
+    function withdraw() external {
+        payable(msg.sender).transfer(deposits[msg.sender]);
+    }
+
+    function allowWithdraw() external onlyAdmin {
+        allowWithdrawal = true;
+    }
+
 }
--- contracts/interfaces/Errors.sol
+++ contracts/interfaces/Errors.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+error NotAdmin(address sender, address admin);
 error LockupPeriodNotOver(uint256 lockupPeriod);
 error QuorumPeriodOver(uint256 quorumPeriod);
 error QuorumPeriodNotOver(uint256 quorumPeriod);
