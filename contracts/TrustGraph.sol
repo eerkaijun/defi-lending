98c235646081afed94ef7b4cd989ac2112a8c587
eerkaijun <kaijuneer@gmail.com>
2022-10-26 13:49:59 +0100

    structure for trust graph

---
+++ contracts/TrustGraph.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import { AlreadyJoinedGraph, InvalidReferrer } from "./interfaces/Errors.sol";
+
+contract TrustGraph is ERC721URIStorage {
+
+    uint256 public currentTokenID = 1;
+    mapping(address => bool) public graphNodes;
+
+    constructor() ERC721("Trust Graph", "<->") {} 
+
+    function joinGraph(bytes memory signature) external {
+        if (graphNodes[msg.sender]) {
+            revert AlreadyJoinedGraph(msg.sender);
+        }
+
+        // user can only join through referral
+        // referral link is in the form of signature
+        bytes32 hashedMessage = keccak256(abi.encodePacked(
+            "\x19Ethereum Signed Message:\n32",
+            msg.sender
+        ));
+        address referrer = getAddressFromSignature(hashedMessage, signature);
+        if (!graphNodes[referrer]) {
+            revert InvalidReferrer();
+        }
+
+        _mint(msg.sender, currentTokenID);
+        currentTokenID ++;
+
+        emit newEdge(msg.sender, referrer);
+    }
+
+    function getAddressFromSignature(bytes32 hashedMessage, bytes memory signature) private pure returns(address) {
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+
+        // Check the signature length
+        if (signature.length != 65) return address(0);
+
+        // Divide the signature in r, s and v variables
+        assembly {
+            r := mload(add(signature, 0x20))
+            s := mload(add(signature, 0x40))
+            v := byte(0, mload(add(signature, 0x60)))
+        }
+
+        // Appendix F in the Ethereum Yellow paper states that the valid range for s is 0 < s < secp256k1n ÷ 2 + 1
+        // Check taken from https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/cryptography/ECDSA.sol#L161
+        if (uint256(s) > 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0) {
+            return address(0);
+        }
+
+        // Version of signature should be 27 or 28, but 0 and 1 are also possible versions
+        if (v < 27) v += 27;
+
+        // If the version is correct return the signer address
+        if (v != 27 && v != 28) {
+            return address(0);
+        } else {
+            return ecrecover(hashedMessage, v, r, s);
+        }
+    }
+
+    event newEdge(address source, address destination);
+}
--- contracts/interfaces/Errors.sol
+++ contracts/interfaces/Errors.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+// Lending protocol
 error NotAdmin(address sender, address admin);
 error NotCapitalPool();
 error LockupPeriodNotOver(uint256 lockupPeriod);
@@ -8,3 +9,7 @@ error QuorumPeriodOver(uint256 quorumPeriod);
 error QuorumPeriodNotOver(uint256 quorumPeriod);
 error QuorumAchieved(uint256 raisedCapital, uint256 quorum); 
 error QuorumNotAchieved(uint256 raisedCapital, uint256 quorum);
+
+// Trust graph
+error AlreadyJoinedGraph(address user);
+error InvalidReferrer();
