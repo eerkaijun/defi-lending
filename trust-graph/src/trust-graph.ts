e30d9ef4d07c77bddba773780c630958636a25a9
eerkaijun <kaijuneer@gmail.com>
2022-11-02 16:52:00 +0000

    enable graph data indexing

--- trust-graph/src/trust-graph.ts
+++ trust-graph/src/trust-graph.ts
@@ -6,61 +6,28 @@ import {
   Transfer,
   newEdge
 } from "../generated/TrustGraph/TrustGraph"
-import { ExampleEntity } from "../generated/schema"
+import { GraphEdge } from "../generated/schema"
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.balanceOf(...)
-  // - contract.currentTokenID(...)
-  // - contract.getApproved(...)
-  // - contract.graphNodes(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.name(...)
-  // - contract.ownerOf(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.tokenURI(...)
-}
+export function handleApproval(event: Approval): void {}
 
 export function handleApprovalForAll(event: ApprovalForAll): void {}
 
 export function handleTransfer(event: Transfer): void {}
 
-export function handlenewEdge(event: newEdge): void {}
+export function handlenewEdge(event: newEdge): void {
+  // Use proper id to load an entity from store
+  const id = event.transaction.hash.toHex();
+  let edge = GraphEdge.load(id);
+
+  // Create the entity if it doesn't already exist
+  if (!edge) {
+    edge = new GraphEdge(id);
+  }
+
+  // Set updated fields to entity
+  edge.source = event.params.source;
+  edge.destination = event.params.destination;
+
+  // Save updated entity to store
+  edge.save();
+}
