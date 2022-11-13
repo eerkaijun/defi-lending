5c8cbc9f8223216525c4aeadfde540a4f51b5d25
eerkaijun <kaijuneer@gmail.com>
2022-11-13 11:05:52 +0000

    polygon deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,6 +5,10 @@ require('dotenv').config();
 module.exports = {
   solidity: "0.8.17",
   networks: {
+    polygon: {
+      url: `https://polygon-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY_POLYGON}`,
+      accounts: [process.env.PRIVATE_KEY]
+    },
     goerli: {
       url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
       accounts: [process.env.PRIVATE_KEY]
